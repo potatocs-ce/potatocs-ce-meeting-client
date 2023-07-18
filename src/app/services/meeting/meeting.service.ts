45372743e7a0bb9357a607b6ef73b62c9045a5fa
potatocs-ce <ce.potatocs@gmail.com>
2023-07-18 17:30:14 +0900

    Update webrtc list

--- src/app/services/meeting/meeting.service.ts
+++ src/app/services/meeting/meeting.service.ts
@@ -93,11 +93,4 @@ export class MeetingService {
 
   ////////////////////////////////////////////////////
 
-  updateMeetingInfo(body) {
-    return this.http.patch('/apim/v1/collab/patchMeeingInfo', body)
-  }
-
-  updateParticipantMeetingInfo(body) {
-    return this.http.patch('/apim/v1/collab/updateParticipantMeetingInfo', body)
-  }
 }
