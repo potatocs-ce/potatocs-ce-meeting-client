5be85771cc48ae25e54ec982a6fd40e6a813bac7
park9379 <109051896+park9379@users.noreply.github.com>
2023-08-07 18:46:09 +0900

    test socket setting

--- src/app/services/socketio/socketio.service.ts
+++ src/app/services/socketio/socketio.service.ts
@@ -1,24 +1,29 @@
 import { Injectable } from '@angular/core';
-import {io, Socket} from'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 import { environment } from '../../../environments/environment';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SocketioService {
+  // private url = 'http://socket-addr';
+  // private url = 'http://localhost:3000/socketWebRTC';
+  private url = environment.socketUrl;
+  private _socket: Socket; // type: https://stackoverflow.com/questions/47161589/how-to-use-socket-io-client-in-angular-4
+  private _socket_test: Socket;
+  constructor() {
+    // this.socket = io(this.url, {transports: ['websocket']});
+    this._socket = io(this.url + '/socketWebRTC', {
+      transports: ['websocket'],
+      path: '/socketWebRTC',
+    });
+    this._socket_test = io(this.url + '/socketWebRTC', {
+      transports: ['websocket'],
+      path: '/socketWebRTC',
+    });
+    // console.log(this._socket);
+  }
 
-	// private url = 'http://socket-addr';
-	// private url = 'http://localhost:3000/socketWebRTC';
-	private url = environment.socketUrl
-	private _socket: Socket; // type: https://stackoverflow.com/questions/47161589/how-to-use-socket-io-client-in-angular-4
-
-
-	constructor() {
-		// this.socket = io(this.url, {transports: ['websocket']});
-		this._socket = io(this.url+'/socketWebRTC', { transports: ['websocket'], path:'/socketWebRTC' });
-		// console.log(this._socket);
-	}
-
-	get socket() {
-		return this._socket;
-	}
+  get socket() {
+    return this._socket;
+  }
 }
