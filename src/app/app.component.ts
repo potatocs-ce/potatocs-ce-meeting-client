64e2a36ccc8c67de43d0245bd2d8bc95c7c72bb2
limhokyun <lim@nsmarts.co.kr>
2024-04-09 11:02:11 +0900

    [chore] 메뉴 추가 및 toolbar 수정

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './layout/toolbar/toolbar.component';
+import { MenuComponent } from './layout/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ToolbarComponent],
+  imports: [CommonModule, RouterOutlet, ToolbarComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
