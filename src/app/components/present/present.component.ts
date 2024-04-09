5f32f9dc965393023dc2a10e50aa2d336d2216b5
limhokyun <lim@nsmarts.co.kr>
2024-04-09 17:02:17 +0900

    [chore] 아이콘 버튼들 추가

--- src/app/components/present/present.component.ts
+++ src/app/components/present/present.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-present',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatIconModule, MatButtonModule],
   templateUrl: './present.component.html',
   styleUrl: './present.component.scss'
 })
