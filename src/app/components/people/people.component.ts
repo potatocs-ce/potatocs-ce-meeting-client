2e3cdc978c52064082d15b2f569192429fc7b826
limhokyun <lim@nsmarts.co.kr>
2024-04-09 10:36:12 +0900

    [feat] toolbar layout 정리

---
+++ src/app/components/people/people.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-people',
+  standalone: true,
+  imports: [],
+  templateUrl: './people.component.html',
+  styleUrl: './people.component.scss'
+})
+export class PeopleComponent {
+
+}
--- src/app/layout/toolbar/toolbar.component.ts
+++ src/app/layout/toolbar/toolbar.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatIconModule, MatButtonModule, MatRippleModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
