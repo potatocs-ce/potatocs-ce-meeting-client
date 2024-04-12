fbbdf6973a973ac2b0917a039e32330ce08e6a59
limhokyun <lim@nsmarts.co.kr>
2024-04-12 14:53:15 +0900

    [chore] audience에 menu추가

--- src/app/components/Audience/audience-video/audience-video.component.ts
+++ src/app/components/Audience/audience-video/audience-video.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-audience-video',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatMenuModule, MatButtonModule, MatIconModule],
   templateUrl: './audience-video.component.html',
   styleUrl: './audience-video.component.scss'
 })
