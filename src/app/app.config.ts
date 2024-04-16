8ae7dcd1700b5e03db0e18330bab13951c74962e
limhokyun <lim@nsmarts.co.kr>
2024-04-16 09:25:55 +0900

    [feat] 소켓 설정

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+
+const config: SocketIoConfig = { url: 'ws://localhost:3000/', options: { transports: ['websocket'], path: '/socket/' } };
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [importProvidersFrom(SocketIoModule.forRoot(config)), provideRouter(routes), provideAnimationsAsync()]
 };
