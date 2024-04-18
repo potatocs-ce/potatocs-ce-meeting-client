0d8f741a3f9b58be87e8f74df6de904c444d0b03
limhokyun <lim@nsmarts.co.kr>
2024-04-18 11:53:37 +0900

    [feat] 동시 처리 작업중

--- src/app/components/present/present.component.ts
+++ src/app/components/present/present.component.ts
@@ -31,7 +31,7 @@ export class PresentComponent {
   tool: any = { type: 'pen', color: 'black' }
 
   observer_target: any;
-
+  stop: any = null;
   constructor(
     private host: ElementRef,
     private zone: NgZone,
@@ -83,12 +83,93 @@ export class PresentComponent {
         // console.log(this.videoDrawingService.drawVarArray().length)
         const data_canvas: any = document.getElementById('data_canvas');
         const data_context: any = data_canvas.getContext('2d');
+        const drawing_canvas: any = document.getElementById('drawing_canvas');
+        const context: any = drawing_canvas.getContext('2d');
 
         const [firstKey, firstValue]: any = Object.entries(this.videoDrawingService.drawVarArray())[0];
 
+        const data = firstValue[firstValue.length - 1];
+        // this.drawingService.end(data_context,firstValue[firstValue.length - 1].points, firstValue[firstValue.length - 1].tool)
 
-        this.drawingService.end(data_context,
-          firstValue[firstValue.length - 1].points, firstValue[firstValue.length - 1].tool)
+        const pointsLength = data.points.length / 2;
+
+
+        context.lineCap = "round";
+        context.lineJoin = 'round';
+        context.globalAlpha = 1;
+        context.lineWidth = data.tool.width;
+
+        if (data.tool.type === "pen") {
+          context.globalCompositeOperation = 'source-over';
+          context.strokeStyle = data.tool.color;
+          context.fillStyle = data.tool.color;
+        }
+        else if (data.tool.type === "eraser") {
+          context.globalCompositeOperation = 'source-over';
+          context.strokeStyle = "rgba(255, 255, 255, 1)";
+          context.fillStyle = "rgba(255, 255, 255, 1)";
+        }
+        else if (data.tool.type === "highlighter") {
+          context.globalCompositeOperation = 'xor';
+          context.globalAlpha = 0.5;
+          context.lineCap = "square";
+          context.fillStyle = '#ff0';
+          context.strokeStyle = '#ff0';
+        }
+
+
+        if (data.tool.type === "pen" || data.tool.type === "eraser" || data.tool.type === "highlighter") {
+          if (pointsLength < 3) {
+            context.beginPath();
+            context.arc(data.points[0], data.points[1], data.tool.width / 2, 0, Math.PI * 2, !0);
+            context.fill();
+            context.closePath();
+
+            // context.clearRect(0, 0, sourceCanvas.width / scale, sourceCanvas.height / scale);
+            this.drawingService.end(data_context, firstValue[firstValue.length - 1].points, firstValue[firstValue.length - 1].tool)
+
+            // this.dataArray.shift();
+            // this.rxDrawingFunc();
+            return;
+
+          }
+
+          let i = 2;
+
+          this.stop = setInterval(() => {
+            context.beginPath();
+            if (i === 2) {
+              context.moveTo(data.points[0], data.points[1]);
+            }
+            else {
+              context.lineCap = "round";
+              const a = (data.points[2 * (i - 2)] + data.points[2 * (i - 1)]) / 2;
+              const b = (data.points[2 * (i - 2) + 1] + data.points[2 * (i - 1) + 1]) / 2;
+              context.moveTo(a, b);
+            }
+            const c = (data.points[2 * (i - 1)] + data.points[2 * i]) / 2;
+            const d = (data.points[2 * (i - 1) + 1] + data.points[2 * i + 1]) / 2;
+            context.quadraticCurveTo(data.points[2 * (i - 1)], data.points[2 * (i - 1) + 1], c, d);
+            context.stroke();
+            i += 1;
+
+            if (i === pointsLength) {
+              clearInterval(this.stop);
+              this.stop = null;
+
+              // this.dataArray.shift();
+              // context.clearRect(0, 0, sourceCanvas.width / scale, sourceCanvas.height / scale);
+
+              // 최종 target에 그리기
+              this.drawingService.end(data_context, firstValue[firstValue.length - 1].points, firstValue[firstValue.length - 1].tool)
+
+              // // 다음 event 그리기 시작.
+              // this.rxDrawingFunc();
+            }
+
+          }, data.timeDiff / pointsLength);
+        }
+
       }
 
     })
