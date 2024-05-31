1eebc7b104459ea64909589a6e96223ad87a0292
limhokyun <lim@nsmarts.co.kr>
2024-05-31 11:57:35 +0900

    [chore] survey css, ui 기본 작업

--- src/app/components/survey/survey.component.ts
+++ src/app/components/survey/survey.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-survey',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatIconModule, MatButtonModule],
   templateUrl: './survey.component.html',
   styleUrl: './survey.component.scss'
 })
