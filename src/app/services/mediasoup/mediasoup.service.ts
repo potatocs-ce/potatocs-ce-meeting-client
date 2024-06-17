a85f04a08c00cc0395235aaae2c38a40f0dc43d0
limhokyun <lim@nsmarts.co.kr>
2024-06-17 11:55:05 +0900

    [fix] 새로 들어온 멤버도 프로필 이미지 지정

--- src/app/services/mediasoup/mediasoup.service.ts
+++ src/app/services/mediasoup/mediasoup.service.ts
@@ -297,14 +297,14 @@ export class MediasoupService {
           meetingInfo.currentMembers.filter((data: any) => data.online).map((user: any) => {
 
             if (this.toggleService.toggle_video_whiteboard() != 'document' && !this.meetingService.present_user_info()) {
-              this.meetingService.present_user_info.set({ user_id: user.member_id._id, name: user.member_id.name, screen: false })
+              this.meetingService.present_user_info.set({ user_id: user.member_id._id, name: user.member_id.name, screen: false, profile: user.member_id.profile_img })
             } else if (!this.meetingService.users_info().some((users: any) => users.user_id == user.member_id._id) &&
               this.meetingService.present_user_info().user_id != user.member_id._id) {
 
-              this.meetingService.users_info.set([...this.meetingService.users_info(), { user_id: user.member_id._id, name: user.member_id.name, screen: false }])
+              this.meetingService.users_info.set([...this.meetingService.users_info(), { user_id: user.member_id._id, name: user.member_id.name, screen: false, profile: user.member_id.profile_img }])
             }
           })
-          console.log(this.meetingService.present_user_info(), this.meetingService.users_info())
+          // console.log(this.meetingService.present_user_info(), this.meetingService.users_info())
         })
       }
     )
