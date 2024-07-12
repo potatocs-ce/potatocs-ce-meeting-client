b15dc73b08b719e2fec010a4e104a481749abbec
limhokyun <lim@nsmarts.co.kr>
2024-07-12 16:15:35 +0900

    [fix] 판서 전체 지우기 시도 시 받는 쪽에서 에러 나는 문제 해결

--- src/app/services/socket/pdf_drawing/pdf-drawing.service.ts
+++ src/app/services/socket/pdf_drawing/pdf-drawing.service.ts
@@ -59,14 +59,15 @@ export class PdfDrawingService {
 
     this.socket.on('draw:doc_clear', async (data: any) => {
 
-      if (this.docService._docList()[this.docService.lastDocNum()]._id == data.docId && this.docService.pageBuffer()[this.docService.lastDocNum()] == data.page) {
+      this.docService.generateDrawingData(data.result);
+      if (this.docService._docList()[this.docService.lastDocNum()]?._id == data.docId && this.docService.pageBuffer()[this.docService.lastDocNum()] == data.page) {
         const canvas_target: any = document.getElementById('canvasUser');
         const target_context: any = canvas_target.getContext('2d');
         target_context.clearRect(0, 0, canvas_target.width, canvas_target.height);
       }
 
 
-      this.docService.generateDrawingData(data.result);
+
 
     })
   }
