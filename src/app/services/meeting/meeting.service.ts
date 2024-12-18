251766d2f2379ac22464b4dd68ff1bd5452466c2
park9379 <109051896+park9379@users.noreply.github.com>
2024-12-18 17:18:42 +0900

    MeetingService 클래스 주석 추가

--- src/app/services/meeting/meeting.service.ts
+++ src/app/services/meeting/meeting.service.ts
@@ -1,27 +1,33 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, signal } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: "root",
 })
 export class MeetingService {
+	// 클래스 생성자
+	constructor() {}
 
-  constructor() { }
+	// 현재 회의실 ID (회의실을 식별하는 고유한 문자열)
+	meeting_room_id = signal<string>("");
 
-  meeting_room_id = signal<string>('');
-  meeting_room_title = signal<string>('');
+	// 현재 회의실 제목 (회의실 이름)
+	meeting_room_title = signal<string>("");
 
-  meeting_info = signal<any>(undefined);
+	// 회의 정보 (전체적인 회의 상태나 설정 정보를 담을 수 있음)
+	meeting_info = signal<any>(undefined);
 
-  meeting_chat_info = signal<any>(undefined);
+	// 회의 채팅 정보 (채팅 메시지 및 관련 데이터)
+	meeting_chat_info = signal<any>(undefined);
 
-  // 판서 정보 안보여줄 데이터 변수
-  skipList = signal<any>([]);
+	// 판서(화이트보드)에서 제외할 데이터 리스트
+	skipList = signal<any>([]);
 
-  present_user_info = signal<any>(undefined);
-  // 참가 중인 유저들 리스트
-  users_info = signal<any>([]);
+	// 현재 발표자 정보 (현재 발표 중인 사용자에 대한 정보)
+	present_user_info = signal<any>(undefined);
 
-  device_check: any = signal<any>(false);
+	// 현재 참가 중인 사용자들의 정보 리스트
+	users_info = signal<any>([]);
 
-
+	// 디바이스 상태 확인 (예: 카메라, 마이크 등 디바이스의 사용 가능 여부)
+	device_check: any = signal<any>(false);
 }
