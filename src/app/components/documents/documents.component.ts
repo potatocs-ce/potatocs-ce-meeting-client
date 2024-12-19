4f73e7095473d06426ec6cbb9aa53a9c5e7e0b8e
park9379 <109051896+park9379@users.noreply.github.com>
2024-12-19 12:03:41 +0900

    document component 가이드라인 업데이트

--- src/app/components/documents/documents.component.ts
+++ src/app/components/documents/documents.component.ts
@@ -1,24 +1,24 @@
-import { CommonModule } from '@angular/common';
-import { Component, effect } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { DocListComponent } from './doc-list/doc-list.component';
-import { DocumentService } from '../../services/document/document.service';
-import { DocPageComponent } from './doc-page/doc-page.component';
+import { CommonModule } from "@angular/common";
+import { Component, effect } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { DocListComponent } from "./doc-list/doc-list.component";
+import { DocumentService } from "../../services/document/document.service";
+import { DocPageComponent } from "./doc-page/doc-page.component";
 
 @Component({
-  selector: 'app-documents',
-  standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, DocListComponent, DocPageComponent],
-  templateUrl: './documents.component.html',
-  styleUrl: './documents.component.scss'
+	selector: "app-documents",
+	standalone: true,
+	imports: [CommonModule, MatButtonModule, MatIconModule, DocListComponent, DocPageComponent],
+	templateUrl: "./documents.component.html",
+	styleUrl: "./documents.component.scss",
 })
 export class DocumentsComponent {
-  doc: any = [];
+	doc: any = [];
 
-  constructor(private docService: DocumentService) {
-    effect(() => {
-      this.doc = this.docService._doc();
-    })
-  }
+	constructor(private docService: DocumentService) {
+		effect(() => {
+			this.doc = this.docService._doc();
+		});
+	}
 }
