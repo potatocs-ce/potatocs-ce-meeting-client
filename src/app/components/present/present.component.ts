177bed5c3f2be9ae7854add4e7163060fd5aa36a
park9379 <109051896+park9379@users.noreply.github.com>
2024-12-19 11:41:16 +0900

    present component ngAfterViewInit 주석 추가

--- src/app/components/present/present.component.ts
+++ src/app/components/present/present.component.ts
@@ -1,176 +1,169 @@
-import { CommonModule } from '@angular/common';
-import { Component, ElementRef, HostListener, NgZone, effect, untracked } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { VideoService } from '../../services/video/video.service';
-import { CanvasService } from '../../services/canvas/canvas.service';
-import { DrawingService } from '../../services/drawing/drawing.service';
-import { ToolService } from '../../services/tool/tool.service';
-import { BehaviorSubject } from 'rxjs';
-import { MeetingServiceAPI } from '../../api/meeting/meetingAPI.service';
-import { MeetingService } from '../../services/meeting/meeting.service';
-import { Socket } from 'ngx-socket-io';
+import { CommonModule } from "@angular/common";
+import { Component, ElementRef, HostListener, NgZone, effect, untracked } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatMenuModule } from "@angular/material/menu";
+import { VideoService } from "../../services/video/video.service";
+import { CanvasService } from "../../services/canvas/canvas.service";
+import { DrawingService } from "../../services/drawing/drawing.service";
+import { ToolService } from "../../services/tool/tool.service";
+import { BehaviorSubject } from "rxjs";
+import { MeetingServiceAPI } from "../../api/meeting/meetingAPI.service";
+import { MeetingService } from "../../services/meeting/meeting.service";
+import { Socket } from "ngx-socket-io";
 @Component({
-  selector: 'app-present',
-  standalone: true,
-  imports: [CommonModule, MatIconModule, MatButtonModule, MatMenuModule],
-  templateUrl: './present.component.html',
-  styleUrl: './present.component.scss'
+	selector: "app-present",
+	standalone: true,
+	imports: [CommonModule, MatIconModule, MatButtonModule, MatMenuModule],
+	templateUrl: "./present.component.html",
+	styleUrl: "./present.component.scss",
 })
 export class PresentComponent {
+	videoStream: any = undefined;
 
-  videoStream: any = undefined;
+	isWidth: boolean | undefined = undefined;
 
-  isWidth: boolean | undefined = undefined;
+	zoomScale: number = 1;
+	firstRender: boolean = true;
 
-  zoomScale: number = 1;
-  firstRender: boolean = true;
+	videoWidth: number | null = 0;
+	videoHeight: number | null = 0;
 
-  videoWidth: number | null = 0;
-  videoHeight: number | null = 0;
+	tool: any = { type: "pen", color: "black" };
 
-  tool: any = { type: 'pen', color: 'black' }
+	observer_target: any;
+	screen: any;
 
-  observer_target: any;
-  screen: any;
+	constructor(
+		private host: ElementRef,
+		private zone: NgZone,
+		private videoService: VideoService,
+		private canvasService: CanvasService,
+		private drawingService: DrawingService,
+		private toolService: ToolService,
+		private meetingApiService: MeetingServiceAPI,
+		private meetingService: MeetingService,
+		private socket: Socket
+	) {
+		effect(() => {
+			this.videoStream = this.meetingService.present_user_info();
 
-  constructor(
-    private host: ElementRef,
-    private zone: NgZone,
-    private videoService: VideoService,
-    private canvasService: CanvasService,
-    private drawingService: DrawingService,
-    private toolService: ToolService,
-    private meetingApiService: MeetingServiceAPI,
-    private meetingService: MeetingService,
-    private socket: Socket) {
-    effect(() => {
-      this.videoStream = this.meetingService.present_user_info()
+			if (this.videoStream == undefined) {
+				// this.observer.unobserve(document.getElementsByClassName('present_container')[0]);
+				const present: any = document.getElementById("present");
 
-      if (this.videoStream == undefined) {
+				present.style.width = "100%";
+				present.style.height = "100%";
 
-        // this.observer.unobserve(document.getElementsByClassName('present_container')[0]);
-        const present: any = document.getElementById('present');
+				this.videoWidth = 0;
+				this.videoHeight = 0;
 
-        present.style.width = '100%';
-        present.style.height = '100%';
+				this.zoomScale = 1;
+				this.firstRender = true;
+			} else {
+				// 이거 안해주니까 뭔가 동작을 안함....
+				const elem: any = document.getElementById("present_video");
+				if (elem) {
+					elem.playsInline = true;
+					elem.autoplay = true;
+					elem.muted = true;
+				}
+			}
+		});
 
-        this.videoWidth = 0;
-        this.videoHeight = 0;
+		effect(() => {
+			this.meetingService.skipList();
+		});
+	}
+	width$ = new BehaviorSubject<number>(0);
+	observer: any;
+	ngAfterViewInit() {
+		// 컴포넌트의 뷰가 초기화된 후 호출되는 Angular 라이프사이클 메서드.
+		// ResizeObserver를 사용하여 DOM 요소 크기 변경을 감지.
 
-        this.zoomScale = 1;
-        this.firstRender = true;
+		this.observer = new ResizeObserver((entries) => {
+			// Angular Zone 안에서 실행하여 변경 사항을 Angular의 변경 감지에 반영.
+			this.zone.run(() => {
+				// 'present_video' ID를 가진 비디오 DOM 요소를 가져옴.
+				const video_target: any = document.getElementById("present_video");
 
-      } else {
-        // 이거 안해주니까 뭔가 동작을 안함....
-        const elem: any = document.getElementById('present_video');
-        if (elem) {
-          elem.playsInline = true;
-          elem.autoplay = true;
-          elem.muted = true;
-        }
-      }
-    })
+				// 비디오 크기 조정을 수행하는 메서드 호출.
+				this.videoResize(video_target);
+			});
+		});
 
-    effect(() => {
-      this.meetingService.skipList()
+		this.observer_target = document.getElementsByClassName("present_container")[0];
 
+		this.observer.observe(this.observer_target);
+	}
 
-    })
-  }
-  width$ = new BehaviorSubject<number>(0);
-  observer: any;
+	ngOnDestroy() {
+		this.observer.unobserve(this.observer_target);
+	}
 
-  ngAfterViewInit() {
-    this.observer = new ResizeObserver(entries => {
-      this.zone.run(() => {
-        const video_target: any = document.getElementById('present_video');
+	/**
+	 * 비디오 크기 변경 = 비디오가 바뀌거나 새로 들어왔을 경우
+	 * @param target 비디오 태그
+	 */
+	videoResize(target: any) {
+		const present: any = document.getElementById("present");
+		// const present_section: any = document.getElementById('present_section');
 
-        this.videoResize(video_target)
-      });
-    });
+		// canvas
+		present.style.width = "100%";
+		present.style.height = "100%";
 
-    this.observer_target = document.getElementsByClassName('present_container')[0]
+		if (this.videoWidth !== target?.videoWidth && this.videoHeight !== target?.videoHeight) {
+			this.videoWidth = target?.videoWidth;
+			this.videoHeight = target?.videoHeight;
+			this.firstRender = true;
+		}
 
-    this.observer.observe(this.observer_target);
-  }
+		this.isWidth = undefined;
 
-  ngOnDestroy() {
-    this.observer.unobserve(this.observer_target);
-  }
+		const pw = present.clientWidth;
+		const ph = present.clientHeight;
 
-  /**
-   * 비디오 크기 변경 = 비디오가 바뀌거나 새로 들어왔을 경우
-   * @param target 비디오 태그
-   */
-  videoResize(target: any) {
+		const vw = target.clientWidth;
+		const vh = target.clientHeight;
 
+		if (ph < vh + (pw - vw)) {
+			// 세로가 길면
+			target.style.height = `${ph}px`;
+			target.style.width = "auto";
+			present.style.width = "fit-content";
+		} else {
+			// 가로가 길면
+			target.style.width = `${pw}px`;
+			target.style.height = "auto";
+			present.style.height = "fit-content";
+		}
+	}
 
-    const present: any = document.getElementById('present');
-    // const present_section: any = document.getElementById('present_section');
+	/**
+	 * 윈도우 화면 변화 감지 함수
+	 */
+	@HostListener("window:resize", ["$event"])
+	onResize() {
+		const video_target: any = document.getElementById("present_video");
+		this.videoResize(video_target);
+	}
 
-    // canvas
-    present.style.width = '100%';
-    present.style.height = '100%';
+	/**
+	 * 비디오 화면 캡쳐 함수
+	 * @param e video tag
+	 */
+	capture(e: any) {
+		// console.log(e);
+		const canvas = document.createElement("canvas");
+		canvas.width = e.clientWidth;
+		canvas.height = e.clientHeight;
 
-    if (this.videoWidth !== target?.videoWidth && this.videoHeight !== target?.videoHeight) {
-
-      this.videoWidth = target?.videoWidth;
-      this.videoHeight = target?.videoHeight;
-      this.firstRender = true;
-    }
-
-    this.isWidth = undefined
-
-
-
-
-    const pw = present.clientWidth;
-    const ph = present.clientHeight;
-
-    const vw = target.clientWidth;
-    const vh = target.clientHeight;
-
-    if (ph < vh + (pw - vw)) {
-      // 세로가 길면
-      target.style.height = `${ph}px`;
-      target.style.width = 'auto';
-      present.style.width = 'fit-content';
-    } else {
-      // 가로가 길면
-      target.style.width = `${pw}px`;
-      target.style.height = 'auto';
-      present.style.height = 'fit-content';
-    }
-
-  }
-
-  /**
-   * 윈도우 화면 변화 감지 함수
-   */
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    const video_target: any = document.getElementById('present_video');
-    this.videoResize(video_target)
-  }
-
-
-  /**
-   * 비디오 화면 캡쳐 함수
-   * @param e video tag
-   */
-  capture(e: any) {
-    // console.log(e);
-    const canvas = document.createElement('canvas');
-    canvas.width = e.clientWidth;
-    canvas.height = e.clientHeight;
-
-    canvas.getContext('2d')?.drawImage(e, 0, 0, canvas.width, canvas.height);
-    const dataURL = canvas.toDataURL('image/png');
-    const a = document.createElement('a');
-    a.href = dataURL;
-    a.download = 'capture.png';
-    a.click();
-  }
+		canvas.getContext("2d")?.drawImage(e, 0, 0, canvas.width, canvas.height);
+		const dataURL = canvas.toDataURL("image/png");
+		const a = document.createElement("a");
+		a.href = dataURL;
+		a.download = "capture.png";
+		a.click();
+	}
 }
