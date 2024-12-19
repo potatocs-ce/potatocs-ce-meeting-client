920edc25ce74f0e52fb55b485642c2977eebbb41
park9379 <109051896+park9379@users.noreply.github.com>
2024-12-19 11:26:13 +0900

    auto guard 코드 가독성을 위한 포맷 수정

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,30 +1,24 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from '../services/auth/auth.service';
+import { inject } from "@angular/core";
+import { CanActivateFn, Router } from "@angular/router";
+import { AuthService } from "../services/auth/auth.service";
 
 export const authGuard: CanActivateFn = (route, state) => {
+	const auth = inject(AuthService);
+	const router = inject(Router);
 
+	const routePath = route.routeConfig?.path;
 
-  const auth = inject(AuthService);
-  const router = inject(Router);
-
-
-  const routePath = route.routeConfig?.path;
-
-  if (!auth.isAuthenticated()) {
-
-    if (routePath == '' || routePath == 'sign-in' || routePath == 'sign-up') {
-
-      return true;
-    } else {
-      // 
-      router.navigate(['/sign-in'], { queryParams: { params: state.url } })
-    }
-  } else {
-    if (routePath == '' || routePath == 'sign-in' || routePath == 'sign-up') {
-
-      window.history.back()
-    }
-  }
-  return true;
+	if (!auth.isAuthenticated()) {
+		if (routePath == "" || routePath == "sign-in" || routePath == "sign-up") {
+			return true;
+		} else {
+			//
+			router.navigate(["/sign-in"], { queryParams: { params: state.url } });
+		}
+	} else {
+		if (routePath == "" || routePath == "sign-in" || routePath == "sign-up") {
+			window.history.back();
+		}
+	}
+	return true;
 };
